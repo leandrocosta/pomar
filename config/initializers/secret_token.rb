2b4059e202d56746bfdf51b1e09bbe96394d86b5
Leandro Souza Costa <leandro.costa@gmail.com>
2010-11-10 19:25:03 -0200

    Initial directory structure using Rails 3.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomar::Application.config.secret_token = 'af1b8336b7c65dcf6bb3075d90cb1db6d74c672bb00a23faa65e181702ef73a29397eaf470d2bbbd614f0ea9436fa0e5d6501e886423d210b3d04bd7462ea614'
