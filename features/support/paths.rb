403c5241f6a54b7fa606b21981cf11e3261ac091
Leandro Souza Costa <leandro.costa@gmail.com>
2010-11-10 20:08:26 -0200

    First feature: task!

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,6 +10,9 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
+    when /the new task page/
+      new_task_path
+
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
