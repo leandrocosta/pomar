f0d7e3799a289ec43d214bbdc25b5ceae336dccf
Leandro Souza Costa <leandro.costa@gmail.com>
2010-12-02 20:53:09 -0200

    Specs for /signup and UsersController

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Pomar: Sign Up!")
+  end
+end
