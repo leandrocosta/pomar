a9e737ecbc02f038242682a7af7af6f7f01aaac4
Leandro Souza Costa <leandro.costa@gmail.com>
2010-12-15 15:50:16 -0200

    Checking if user is authenticated: features and specs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,24 @@
 class SessionsController < ApplicationController
-  def new
-    @title = "Pomar: Sign In!"
-  end
+	def new
+		@title = "Pomar: Sign In!"
+	end
 
-  def create
-    if (params[:username].blank? || params[:password].blank?)
-      flash.now[:alert] = "Invalid username/password combination"
-      render :action => 'new'
-    else
-		session[:user_id] = 1
-      redirect_to main_path, :notice => "You are logged in!"
-    end
-  end
+	def create
+		user = User.authenticate(params[:username], params[:password]);
 
-  def destroy
-	  reset_session
-	  redirect_to '/', :notice => "Your are logged out"
-  end
+		#if (params[:username].blank? || params[:password].blank?)
+		if (user.blank?)
+			flash.now[:alert] = "Invalid username/password combination"
+			render :action => 'new'
+		else
+			#session[:user_id] = 1
+			session[:user_id] = user.id
+			redirect_to main_path, :notice => "You are logged in!"
+		end
+	end
+
+	def destroy
+		reset_session
+		redirect_to '/', :notice => "Your are logged out"
+	end
 end
