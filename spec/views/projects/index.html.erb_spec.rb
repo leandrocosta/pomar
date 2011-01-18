fabf602d87cd7610fccc48957993159f901e0f77
Leandro Souza Costa <leandro.costa@gmail.com>
2011-01-18 14:12:26 -0200

    CRUD sketch for projects

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -1,14 +1,4 @@
 require 'spec_helper'
 
 describe "projects/index.html.erb" do
-  before(:each) do
-    assign(:projects, [
-      stub_model(Project),
-      stub_model(Project)
-    ])
-  end
-
-  it "renders a list of projects" do
-    render
-  end
 end
